d93c4a88eca0a0b2f8d80415fa1b2cffa9b27221
Unknown <meemnishat@gmail.com>
2017-09-16 22:07:13 +0600

    Hello

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Medilab Free Bootstrap HTML5 Template</title>
+    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- =======================================================
+        Theme Name: Medilab
+        Theme URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+        Author: BootstrapMade.com
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+  </head>
+  <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <!--banner-->
+    <section id="banner" class="banner">
+        <div class="bg-color">
+            <nav class="navbar navbar-default navbar-fixed-top">
+              <div class="container">
+                <div class="col-md-12">
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+                    </div>
+                    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                      <ul class="nav navbar-nav">
+                        <li class="active"><a href="#banner">Home</a></li>
+                        <li class=""><a href="#service">Services</a></li>
+                        <li class=""><a href="#about">About</a></li>
+                        <li class=""><a href="#testimonial">Testimonial</a></li>
+                        <li class=""><a href="#contact">Contact</a></li>
+                      </ul>
+                    </div>
+                </div>
+              </div>
+            </nav>
+            <div class="container">
+                <div class="row">
+                    <div class="banner-info">
+                        <div class="banner-logo text-center">
+                            <img src="img/logo.png" class="img-responsive">
+                        </div>
+                        <div class="banner-text text-center">
+                            <h1 class="white">Healthcare at your desk!!</h1>
+                            <p>A cheaper alternative to hotel accommodation or serviced apartments.</p>
+                            <a href="#contact" class="btn btn-appoint">Make an Appointment.</a>
+                        </div>
+                        <div class="overlay-detail text-center">
+                           <a href="#service"><i class="fa fa-angle-down"></i></a>
+                         </div>     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ banner-->
+    <!--service-->
+    <section id="service" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <h2 class="ser-title">Our Service</h2>
+                    <hr class="botm-line">
+                    <p>We provide short let which is a property that can be let from a few days to a few weeks.All our short let properties are furnished to a high standard with appliances and household items such as towels, linen, crockery and electrical goods are supplied.Its suitable for students, professionals and tourists visiting London.</p>
+                </div>
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="service-info">
+                        <div class="icon">
+                            <i class="fa fa-home"></i>
+                        </div>
+                        <div class="icon-info">
+                            <h4>Home Comforts</h4>
+                            <p>The privacy of your own front door, plenty of space and a relaxing environment where you can come and go as you please.</p>
+                        </div>
+                    </div>
+                    <div class="service-info">
+                        <div class="icon">
+                            <i class="fa fa-warning"></i>
+                        </div>
+                        <div class="icon-info">
+                            <h4>Do not disturb!</h4>
+                            <p>It is like your second home.You are not confined to one room, do not have to share breakfast with strangers and won't be disturbed by noisy guest</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="service-info">
+                        <div class="icon">
+                            <i class="fa fa-binoculars"></i>
+                        </div>
+                        <div class="icon-info">
+                            <h4>Experience</h4>
+                            <p>You will have your own front door and the opportunity not to just visit London but to really live there!</p>
+                        </div>
+                    </div>
+                    <div class="service-info">
+                        <div class="icon">
+                            <i class="fa fa-calendar-check-o"></i>
+                        </div>
+                        <div class="icon-info">
+                            <h4>Flexibility</h4>
+                            <p>You have the option to extend your tenancy on a weekly or monthly basis.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ service-->
+    
+    
+    
+    <!--footer-->
+    <footer id="footer">
+        <div class="top-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 marb20">
+                            <div class="ftr-tle">
+                                <h4 class="white no-padding">Contact Us</h4>
+                            </div>
+                            <div class="info-sec">
+                                 <p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>321 Awesome Street<br>
+                                    New York, NY 17022</p>
+                                  <p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>+1 800 123 1234</p>
+                            </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 marb20">
+                        <div class="ftr-tle">
+                            <h4 class="white no-padding">Email</h4>
+                        </div>
+                        <div class="info-sec">
+                            <p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i>info@companyname.com</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 marb20">
+                        <div class="ftr-tle">
+                            <h4 class="white no-padding">Follow us</h4>
+                        </div>
+                        <div class="info-sec">
+                            <ul class="social-icon">
+                                <li class="bglight-blue"><i class="fa fa-facebook"></i></li>
+                                <li class="bgred"><i class="fa fa-google-plus"></i></li>
+                                <li class="bgdark-blue"><i class="fa fa-linkedin"></i></li>
+                                <li class="bglight-blue"><i class="fa fa-twitter"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-line">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        © Copyright Medilab Theme. All Rights Reserved
+                        <div class="credits">
+                            <!-- 
+                                All the links in the footer should remain intact. 
+                                You can delete the links only if you purchased the pro version.
+                                Licensing information: https://bootstrapmade.com/license/
+                                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Medilab
+                            -->
+                            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer-->
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="contactform/contactform.js"></script>
+    
+  </body>
+</html>
