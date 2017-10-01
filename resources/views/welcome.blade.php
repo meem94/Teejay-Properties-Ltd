0cc9c458368b61a84593d7bf55e8507bf61e8127
MM <mahirhasancse@gmail.com>
2017-10-01 13:41:07 +0600

    First Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Medilab Free Bootstrap HTML5 Template</title>
+    <title>Teejay Properties Ltd.</title>
     <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
     <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
     
@@ -39,7 +39,7 @@
                         <li class=""><a href="#service">Services</a></li>
                         <li class=""><a href="#about">About</a></li>
                         <li class=""><a href="#testimonial">Testimonial</a></li>
-                        <li class=""><a href="#contact">Contact</a></li>
+                        <li class=""><a href="#footer">Contact</a></li>
                       </ul>
                     </div>
                 </div>
